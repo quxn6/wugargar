d8b5e4e3cd072b761224f52667a6efc9b4cc1f38
SunghwanKim <quxn6@naver.com>
2013-11-06 00:36:51 -0800

    경찰의 감염 회피확률 제거

--- Direct2D_Sample/D2D/Police.h
+++ Direct2D_Sample/D2D/Police.h
@@ -14,12 +14,8 @@ public:
 
 public:	
 	bool IsInfected() {return m_infected;};
-	float GetInfectingEvationRate() {return m_infectingEvationRate;};
-	
-	void SetInfectingEvationRate(float IER) { m_infectingEvationRate = IER;};
 	void SetInfectedStatus(bool infected) {m_infected = infected;};
 
 private:
 	bool m_infected;
-	float m_infectingEvationRate;
 };
