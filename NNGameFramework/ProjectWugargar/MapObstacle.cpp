1cc466383bc3d46c18d7ef936bfe0b17b05ca78f
chunginho <inho9377@gmail.com>
2013-11-26 01:04:21 -0800

    ClickArea로직 추가

--- NNGameFramework/ProjectWugargar/MapObstacle.cpp
+++ NNGameFramework/ProjectWugargar/MapObstacle.cpp
@@ -49,7 +49,7 @@ void CMapObstacle::InitSprite(std::wstring imagePath )
 	m_pObstacle_sprite = NNSprite::Create(imagePath);
 
 	m_pObstacle_sprite->SetPosition(RandomMapPosition());
-	AddChild(m_pObstacle_sprite, 10);
+	AddChild(m_pObstacle_sprite);
 
 }
 
@@ -60,13 +60,24 @@ NNPoint CMapObstacle::RandomMapPosition()
 	return (NNPoint(rand() % (MAP_SIZE_X-1) * TILE_SIZE_X, 270 + rand()%(MAP_SIZE_Y-1)*TILE_SIZE_Y));
 }
 
-//임시
+//Area내부가 클릭되었는지 체크
 bool CMapObstacle::CheckClickArea()
 {
-	if(NNInputSystem::GetInstance()->GetKeyState(VK_BACK) == KEY_DOWN)
-	{
-		return true;
-	}
+	NNPoint cursorPosition = NNInputSystem::GetInstance()->GetMousePosition();
 
+	bool isInXCoordRange = (m_pObstacle_sprite->GetPositionX() < cursorPosition.GetX()) && ( ( m_pObstacle_sprite->GetPositionX() + m_pObstacle_sprite->GetImageWidth() ) > cursorPosition.GetX() );
+	bool isInYCoordRange = (m_pObstacle_sprite->GetPositionY() < cursorPosition.GetY()) && ( ( m_pObstacle_sprite->GetPositionY() + m_pObstacle_sprite->GetImageHeight() ) > cursorPosition.GetY() );
+
+	if ( !(isInXCoordRange && isInYCoordRange))
+	{
+		return false;
+	}
+	else
+	{
+		if (NNInputSystem::GetInstance()->GetKeyState( VK_LBUTTON ) == KEY_UP )
+			return true;
+	}
+	
 	return false;
+
 }
