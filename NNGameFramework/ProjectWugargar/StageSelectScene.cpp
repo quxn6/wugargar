6bef7199a94f47d39dfc9ddfd7082a12c5028d82
chunginho <inho9377@gmail.com>
2013-11-29 22:29:47 -0800

    함수 일부 추가

--- NNGameFramework/ProjectWugargar/StageSelectScene.cpp
+++ NNGameFramework/ProjectWugargar/StageSelectScene.cpp
@@ -1,11 +1,26 @@
 #include "StageSelectScene.h"
 
 
-StageSelectScene::StageSelectScene(void)
+CStageSelectScene::CStageSelectScene(void)
 {
 }
 
 
-StageSelectScene::~StageSelectScene(void)
+CStageSelectScene::~CStageSelectScene(void)
 {
+}
+
+void CStageSelectScene::InitMenuButtons( void )
+{
+
+}
+
+void CStageSelectScene::Render()
+{
+	NNObject::Render();
+}
+
+void CStageSelectScene::Update( float dTime )
+{
+
 }
--- NNGameFramework/ProjectWugargar/StageSelectScene.h
+++ NNGameFramework/ProjectWugargar/StageSelectScene.h
@@ -1,10 +1,28 @@
 #pragma once
 #include "nnscene.h"
-class StageSelectScene :
+#include "UIButton.h"
+class CStageSelectScene :
 	public NNScene
 {
 public:
-	StageSelectScene(void);
-	~StageSelectScene(void);
+	CStageSelectScene(void);
+	~CStageSelectScene(void);
+
+public:
+	void InitMenuButtons( void );
+	void Render();
+	void Update( float dTime );
+
+	NNCREATE_FUNC(CStageSelectScene);
+
+private:
+	CUIButton *stage1_button;
+	CUIButton *stage2_button;
+	CUIButton *stage3_button;
+	CUIButton *stage4_button;
+	CUIButton *stage5_button;
+	CUIButton *stage6_button;
+	CUIButton *stage7_button;
+
 };
 
