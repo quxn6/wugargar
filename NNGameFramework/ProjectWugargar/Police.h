3a01e3269dd0cdb9a4748f2fa72ff48510e00745
chunginho <inho9377@gmail.com>
2013-12-01 08:04:48 -0800

    struct추가

--- NNGameFramework/ProjectWugargar/Police.h
+++ NNGameFramework/ProjectWugargar/Police.h
@@ -1,6 +1,20 @@
 #pragma once
 #include "character.h"
 
+//Create할 때, 해당 타입에 맞는 PoliceInfo가 설정되도록
+struct PoliceInfo{
+	float HealthPoint;
+	float HPRatioPer100;
+	float MovingSpeed;
+	float AttackRange;
+	float SplashRange;
+	int ATtackPower;
+	int DefensivePower;
+	int AttackSpeed;
+	bool IsSplash;
+	std::string SpritePath;
+};
+
 class CPolice : public CCharacter
 {
 public:
@@ -15,6 +29,9 @@ public:
 public:	
 	bool IsInfected() {return m_infected;};
 	void SetInfectedStatus(bool infected) {m_infected = infected;};
+
+
+	PoliceInfo policeInfo[5];
 
 protected:
 	bool m_infected;
