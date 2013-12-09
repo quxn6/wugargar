ad5d16df7d79f06dec83889484ca1fbf25633e1f
chunginho <inho9377@gmail.com>
2013-12-08 23:56:21 -0800

    좀비 생성 사운드 추가

--- NNGameFramework/ProjectWugargar/Zombie.cpp
+++ NNGameFramework/ProjectWugargar/Zombie.cpp
@@ -1,9 +1,12 @@
 #include "Zombie.h"
 #include "PlayScene.h"
+#include "NNResourceManager.h"
+#include "NNAudioSystem.h"
 
 
 CZombie::CZombie(void)
 {
+	PlayCreationSound();
 //	enemyList = &(CPlayScene::GetInstance()->GetPoliceList());
 }
 
@@ -31,4 +34,32 @@ void CZombie::ApplyZombieLevel()
 	m_AttackPower += m_AttackPower/5 *currentZombieLevel;
 	m_DefensivePower += m_DefensivePower/5 + currentZombieLevel;
 }
+
+
+//12.09 정인호
+//좀비의 생성시 사운드를 재생하는 함수.
+void CZombie::PlayCreationSound()
+{
+	std::string soundPath;
+
+	//랜덤하게 두 개의 생성 소리 중 하나를 재생하거나
+	//재생을 하지 않는다.
+	switch (rand() % 3)
+	{
+	case 0:
+		soundPath = "sound/ZombieCreateSound1.wav";
+		break;
+	case 1:
+		soundPath = "sound/ZombieCreateSound2.wav";
+		break;
+	default:
+		return;
+	}
+
+	m_creation_sound = NNResourceManager::GetInstance()->LoadSoundFromFile(soundPath, false, false);
+	
+	NNAudioSystem::GetInstance()->Play(m_creation_sound);
+
+}
+
 
--- NNGameFramework/ProjectWugargar/Zombie.h
+++ NNGameFramework/ProjectWugargar/Zombie.h
@@ -19,6 +19,8 @@ public:
 
 	void	ApplyZombieLevel();
 
+	void	PlayCreationSound();
+
 protected:
 	int			m_CreateCost;
 	ZombieType	m_zombieType;
